0ccf38f98e4bda9eba474890624de8e935adbf6b
marco-unibo <marco.dicristina@gmail.com>
2015-07-30 11:17:20 +0200

    correct info header

--- get_nominatim_info.cpp
+++ get_nominatim_info.cpp
@@ -1,20 +1,20 @@
 /* Copyright 2015 - Marco Di Cristina, Alessandro Fabbri, Mattia Guidetti, Stefano Sinigardi */
 
 /***************************************************************************
-This file is part of json_to_html.
+This file is part of get_nominatim_info.
 
-json_to_html is free software : you can redistribute it and / or modify
+get_nominatim_info is free software : you can redistribute it and / or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-json_to_html is distributed in the hope that it will be useful,
+get_nominatim_info is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with json_to_html. If not, see <http://www.gnu.org/licenses/>.
+along with get_nominatim_info. If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************************/
 
 #include "jsoncons/json.hpp"
